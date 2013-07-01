22f8093ce6b5aa378a2890d883ae92c8f7009817
Matt Tschoegl <matt.tschoegl@gmail.com>
2013-07-01 10:04:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoaBlog::Application.config.secret_token = '5df0dab4867b9211fd9e0a055ecfb511eb731813baddc1042ec91e15bdf96faf3529628764cc911d6477d543ce4298a2f48e2465ef8a538b32d4201848b1ea43'
